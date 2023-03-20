be2a227e292217200da76790b3c0dc0bf51b54ac
Ducvv <vuduc197@github.com>
2023-03-20 14:34:59 +0700

    add list

---
+++ src/components/productlist.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Product List</h2>
+    <product-list :products="products" @add-to-cart="addToCart"></product-list>
+  </div>
+</template>
+
+<script>
+import ProductList from './ProductList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'product-list': ProductList
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Product 1',
+          description: 'This is product 1',
+          price: 10
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          description: '
---
+++ src/components/testgit.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class ="product-list">
+
+    </div class="product" v-for="product in products" :key="product.id">
+    <h3> {{ product.name }}</h3>
+    <p> {{ product.description }}</p>
+    <p>Price: {{ product.price }}</p>
+    <button @click="addToCart(product)"> Add to Cart</button>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    addToCart(product) {
+      // emit an event to the parent component to add the product to the cart
+      this.$emit('add-to-cart', product);
+    }
+  }
+};
+</script>
+
+<style>
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.product {
+  flex: 1 1 300px;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
